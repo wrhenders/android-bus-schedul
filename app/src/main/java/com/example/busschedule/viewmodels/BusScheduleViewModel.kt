ac6ca9e53e5a296339f201dd3102c8900cbfb88b
Ryan Henderson <wrhenders@gmail.com>
2022-10-24 09:48:26 -0500

    setting up Room requirements for db integration

--- app/src/main/java/com/example/busschedule/viewmodels/BusScheduleViewModel.kt
+++ app/src/main/java/com/example/busschedule/viewmodels/BusScheduleViewModel.kt
@@ -1,4 +1,24 @@
 package com.example.busschedule.viewmodels
 
-class BusScheduleViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.busschedule.database.schedule.Schedule
+import com.example.busschedule.database.schedule.ScheduleDao
+
+class BusScheduleViewModel(private val scheduleDao: ScheduleDao): ViewModel() {
+    fun fullSchedule(): List<Schedule> = scheduleDao.getAll()
+
+    fun scheduleForStopName(name: String): List<Schedule> = scheduleDao.getByStopName(name)
+}
+
+class BusScheduleViewModelFactory(private val scheduleDao: ScheduleDao)
+    : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(BusScheduleViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return BusScheduleViewModel(scheduleDao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+
 }
